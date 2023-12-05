c8acf5485a9b92fe18afc4d9e0c5a799562174e2
cliff <cliffmudigo5@gmail.com>
2023-12-06 01:45:38 +0300

    :sparkles: feat: mongoose authentication

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB" )
+
+
+const userSchema = {
+    email: "string",
+    password: "string"
+};
+const User = mongoose.model("User", userSchema);
+
+ 
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res )=> {
+    res.render("login");
+});
+
+app.get("/register", (req, res ) => {
+    res.render("register");
+});
+
+
+app.post("/register", (req, res) => {
+    
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save()
+    .then(() => {
+        res.render("secrets");
+    }).catch((err) => {
+        console.log(err);
+    })
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+ 
+    User.findOne({ email: username })
+    .then(foundUser => {
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            } else {
+                res.send("Incorrect password");
+            }
+        } else {
+            res.send("User not found");
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send("Internal Server Error");  
+    });
+
+ 
+})
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
